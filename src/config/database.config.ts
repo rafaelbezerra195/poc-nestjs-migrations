47584e6565e570502f17e27497e8bfa9b077714a
ViCesarDFT <vinicius.cesar@dafiti.com.br>
2023-09-22 15:44:10 -0300

    feat: Implemented Users Controller

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,11 +3,11 @@ import { configuration } from './configuration';
 
 export const databaseConfig: DataSourceOptions = {
   type: 'mysql',
-  host: 'localhost',
+  host: 'mysql',
   port: 3306,
   username: configuration.database.rootUsername,
   password: configuration.database.rootPassword,
   database: 'db',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  migrations: ['./migrations/*.ts'],
+  //migrations: ['./migrations/*.ts'],
 };
